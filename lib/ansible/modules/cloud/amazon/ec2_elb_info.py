dc007d14cb28f76597dbe1a0bc61a69dae73ef09
Xander Harris <xander.harris@infor.com>
2020-04-09 15:19:07 -0700

    Add boto3
    See #7

--- lib/ansible/modules/cloud/amazon/ec2_elb_info.py
+++ lib/ansible/modules/cloud/amazon/ec2_elb_info.py
@@ -13,6 +13,14 @@
 # You should have received a copy of the GNU General Public License
 # along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
+from ansible.module_utils.ec2 import (
+    AWSRetry,
+    connect_to_aws,
+    ec2_argument_spec,
+    get_aws_connection_info,
+)
+from ansible.module_utils.basic import AnsibleModule
+import traceback
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
@@ -78,17 +86,9 @@ EXAMPLES = '''
 
 '''
 
-import traceback
-
-from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.ec2 import (
-    AWSRetry,
-    connect_to_aws,
-    ec2_argument_spec,
-    get_aws_connection_info,
-)
 
 try:
+    import boto3
     import boto.ec2.elb
     from boto.ec2.tag import Tag
     from boto.exception import BotoServerError
@@ -104,7 +104,7 @@ class ElbInformation(object):
                  module,
                  names,
                  region,
-                 **aws_connect_params):
+                 ** aws_connect_params):
 
         self.module = module
         self.names = names
@@ -114,12 +114,15 @@ class ElbInformation(object):
 
     def _get_tags(self, elbname):
         params = {'LoadBalancerNames.member.1': elbname}
-        elb_tags = self.connection.get_list('DescribeTags', params, [('member', Tag)])
-        return dict((tag.Key, tag.Value) for tag in elb_tags if hasattr(tag, 'Key'))
+        elb_tags = self.connection.get_list(
+            'DescribeTags', params, [('member', Tag)])
+        return dict((tag.Key, tag.Value)
+                    for tag in elb_tags if hasattr(tag, 'Key'))
 
     @AWSRetry.backoff(tries=5, delay=5, backoff=2.0)
     def _get_elb_connection(self):
-        return connect_to_aws(boto.ec2.elb, self.region, **self.aws_connect_params)
+        return connect_to_aws(boto.ec2.elb, self.region,
+                              **self.aws_connect_params)
 
     def _get_elb_listeners(self, listeners):
         listener_list = []
@@ -194,15 +197,24 @@ class ElbInformation(object):
             elb_info['vpc_id'] = elb.vpc_id
 
         if elb.instances:
-            instance_health = self.connection.describe_instance_health(elb.name)
-            elb_info['instances_inservice'] = [inst.instance_id for inst in instance_health if inst.state == 'InService']
-            elb_info['instances_inservice_count'] = len(elb_info['instances_inservice'])
-            elb_info['instances_outofservice'] = [inst.instance_id for inst in instance_health if inst.state == 'OutOfService']
-            elb_info['instances_outofservice_count'] = len(elb_info['instances_outofservice'])
+            instance_health = self.connection.describe_instance_health(
+                elb.name)
+            elb_info['instances_inservice'] = [
+                inst.instance_id for inst in instance_health
+                if inst.state == 'InService']
+            elb_info['instances_inservice_count'] = len(
+                elb_info['instances_inservice'])
+            elb_info['instances_outofservice'] = [
+                inst.instance_id for inst in instance_health
+                if inst.state == 'OutOfService']
+            elb_info['instances_outofservice_count'] = len(
+                elb_info['instances_outofservice'])
             try:
                 elb_info['instances_inservice_percent'] = (
                     float(elb_info['instances_inservice_count']) /
-                    float(elb_info['instances_inservice_count'] + elb_info['instances_outofservice_count'])
+                    float(
+                        elb_info['instances_inservice_count'] +
+                        elb_info['instances_outofservice_count'])
                 ) * 100.
             except ZeroDivisionError:
                 elb_info['instances_inservice_percent'] = 0.
@@ -210,7 +222,9 @@ class ElbInformation(object):
 
     def list_elbs(self):
         elb_array, token = [], None
-        get_elb_with_backoff = AWSRetry.backoff(tries=5, delay=5, backoff=2.0)(self.connection.get_all_load_balancers)
+        get_elb_with_backoff = AWSRetry.backoff(
+            tries=5, delay=5, backoff=2.0)(
+            self.connection.get_all_load_balancers)
         while True:
             all_elbs = get_elb_with_backoff(marker=token)
             token = all_elbs.next_marker
@@ -234,32 +248,47 @@ class ElbInformation(object):
 def main():
     argument_spec = ec2_argument_spec()
     argument_spec.update(dict(
-        names={'default': [], 'type': 'list'}
+        names={'default': [], 'type': 'list'},
+        use_boto3={'default': False, 'type': 'bool'}
     )
     )
     module = AnsibleModule(argument_spec=argument_spec,
                            supports_check_mode=True)
     if module._name == 'ec2_elb_facts':
-        module.deprecate("The 'ec2_elb_facts' module has been renamed to 'ec2_elb_info'", version='2.13')
+        module.deprecate(
+            "The 'ec2_elb_facts' module has been renamed to 'ec2_elb_info'",
+            version='2.13')
 
     if not HAS_BOTO:
         module.fail_json(msg='boto required for this module')
 
-    try:
-        region, ec2_url, aws_connect_params = get_aws_connection_info(module)
-        if not region:
-            module.fail_json(msg="region must be specified")
-
-        names = module.params['names']
-        elb_information = ElbInformation(
-            module, names, region, **aws_connect_params)
-
+    if module.params['use_boto3']:
+        print('use boto3')
+        session = boto3.session.Session(
+            profile_name=module.params['profile'],
+            region_name=module.params['region'])
+        client = session.client('elb')
+        elbs = client.describe_load_balancers()
+        print(elbs)
         ec2_info_result = dict(changed=False,
-                               elbs=elb_information.list_elbs())
+                               elbs=elbs)
+    else:
+        try:
+            region, ec2_url, aws_connect_params = get_aws_connection_info(
+                module)
+            if not region:
+                module.fail_json(msg="region must be specified")
 
-    except BotoServerError as err:
-        module.fail_json(msg="{0}: {1}".format(err.error_code, err.error_message),
-                         exception=traceback.format_exc())
+            names = module.params['names']
+            elb_information = ElbInformation(
+                module, names, region, **aws_connect_params)
+
+            ec2_info_result = dict(changed=False,
+                                   elbs=elb_information.list_elbs())
+
+        except BotoServerError as err:
+            module.fail_json(msg="{0}: {1}".format(err.error_code, err.error_message),
+                             exception=traceback.format_exc())
 
     module.exit_json(**ec2_info_result)
 
